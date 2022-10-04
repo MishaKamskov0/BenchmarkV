6d4f85300eea62921dc89ea1eba79495964fc1c1
Misha Kamskov <mobile0just@gmail.com>
2022-10-04 15:10:33 +0300

    Adding ViewPager & ViewPagerAdapter

--- app/src/main/java/com/example/benchmarkv/MainActivity.java
+++ app/src/main/java/com/example/benchmarkv/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.benchmarkv;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mainTabLayout;
+    private ViewPager mainViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createViewPager();
+    }
+
+    private void createViewPager(){
+        mainViewPager = findViewById(R.id.mainViewPager);
+        mainViewPager.setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
+
+        mainTabLayout = findViewById(R.id.mainTabLayout);
+        mainTabLayout.setupWithViewPager(mainViewPager);
     }
 }
---
+++ app/src/main/java/com/example/benchmarkv/ViewPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.benchmarkv;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.benchmarkv.ui_fragments.CollectionFragment;
+import com.example.benchmarkv.ui_fragments.MapFragment;
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private String[] namesOfTabs = {"Collections", "Maps"};
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position) {
+            case 1:
+                return new MapFragment();
+        }
+        return new CollectionFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return namesOfTabs.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return namesOfTabs[position];
+    }
+}
